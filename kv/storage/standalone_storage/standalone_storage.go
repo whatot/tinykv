87b111ebfd4375489893a81dff1de2f2aadfeb62
whatot <whatot2@gmail.com>
2020-05-27 00:26:34 +0800

    impl AloneStorageReader GetCF and close

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -1,6 +1,8 @@
 package standalone_storage
 
 import (
+	"errors"
+
 	"github.com/Connor1996/badger"
 	"github.com/pingcap-incubator/tinykv/kv/config"
 	"github.com/pingcap-incubator/tinykv/kv/storage"
@@ -38,8 +40,11 @@ func (s *StandAloneStorage) Stop() error {
 }
 
 func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader, error) {
-	// Your Code Here (1).
-	return nil, nil
+	if s.db == nil {
+		return nil, errors.New("db haven't started yet")
+	}
+
+	return &AloneStorageReader{s.db, 0}, nil
 }
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
@@ -56,4 +61,40 @@ func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) 
 	}
 
 	return write_batch.WriteToDB(s.db)
+}
+
+type AloneStorageReader struct {
+	inner     *badger.DB
+	iterCount int
+}
+
+func (r *AloneStorageReader) GetCF(cf string, key []byte) ([]byte, error) {
+	var res []byte
+	err := r.inner.View(func(txn *badger.Txn) error {
+		item, err1 := txn.Get(engine_util.KeyWithCF(cf, key))
+		if err1 != nil {
+			return err1
+		}
+		v, err2 := item.Value()
+		if err2 != nil {
+			return err2
+		}
+		res = v
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func (r *AloneStorageReader) IterCF(cf string) engine_util.DBIterator {
+	// TODO impl iter
+	return nil
+}
+
+func (r *AloneStorageReader) Close() {
+	if r.iterCount > 0 {
+		panic("Unclosed iterator")
+	}
 }
