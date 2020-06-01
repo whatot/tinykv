5fff10b57a5dca7ef5271ef80d18521a4d5cd559
whatot <whatot2@gmail.com>
2020-06-01 22:05:27 +0800

    fix rawDelete test "should return not found error"

--- kv/server/server_test.go
+++ kv/server/server_test.go
@@ -184,7 +184,7 @@ func TestRawDelete1(t *testing.T) {
 	assert.Nil(t, err)
 
 	val, err := Get(s, cf, []byte{99})
-	assert.Equal(t, nil, err)
+	assert.NotNil(t, err)
 	assert.Equal(t, []byte(nil), val)
 }
 
